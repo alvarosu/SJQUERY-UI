48c0db5fea59319a5ee4efac3eceaf87beb19c99
Álvaro Suárez <alvarosu@users.noreply.github.com>
2014-03-05 15:48:57 +0100

    Show & Hide with CSS

--- utils.js
+++ utils.js
@@ -1,19 +1,24 @@
 $(document).ready(function () {
     //ACCORDION
     /**
-      *
+      * ---- HTML ----
+      * 
       * <div class="accordion">
       *   <h2 class="accordion-header"></h2>
       *   <div class="accordion-content"></div>
       * </div>
+      * 
+      * ---- CSS ----
       *
+      * .accordion-content{ display: none; }
+      * .accordion-content.active{ display: block; }
+      * 
       **/
     $(".accordion").each(function (index, value) {
         $(this).attr("id", "accordion-" + index);
-        $("#accordion-" + index).find(".accordion-content").hide();
         $("#accordion-" + index).on("click", ".accordion-header", function () {
-            $("#accordion-" + index).find(".accordion-content").hide();
-            $(this).next(".accordion-content").show();
+            $("#accordion-" + index).find(".accordion-content").removeClass("active");
+            $(this).next(".accordion-content").addClass("active");
         });
     });
 });
