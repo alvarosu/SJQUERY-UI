599d3648aebe7201f790f2ee8bee8cc238641900
Álvaro Suárez <alvarosu@users.noreply.github.com>
2014-03-13 17:13:50 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -21,4 +21,20 @@ $(document).ready(function () {
             $(this).next(".accordion-content").addClass("active");
         });
     });
+    
+    //ROTATE IMAGES
+    /**
+      * 
+      * 
+      *
+      **/
+    $("#slideshow > div:gt(0)").hide();
+    setInterval(function() { 
+      $('#slideshow > div:first')
+        .fadeOut(1000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo('#slideshow');
+    },  5000);
 });
